59cbdc8fefaac3b10d0a909e100272a1a74e7dea
Sachin Naik <sacnaik1@publicisgroupe.net>
2022-06-29 08:35:45 +0530

    Fixing lang Attribute

--- next.config.js
+++ next.config.js
@@ -5,9 +5,8 @@ const nextConfig = {
 }
 
 module.exports = {nextConfig ,images: {
-  domains: ['m.media-amazon.com','assets.myntassets.com','5.imimg.com','rukminim1.flixcart.com','assets.ajio.com'],i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  
+  domains: ['m.media-amazon.com','assets.myntassets.com','5.imimg.com','rukminim1.flixcart.com','assets.ajio.com']
+},i18n: {
+  locales: ["en"],
+  defaultLocale: "en",
 },} 
