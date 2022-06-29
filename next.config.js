8f8ff243492e1e2ac77a643bb1e9158d17442c81
Sachin Naik <sacnaik1@publicisgroupe.net>
2022-06-29 08:32:53 +0530

    lang attribute

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   
 }
 
 module.exports = {nextConfig ,images: {
-  domains: ['m.media-amazon.com','assets.myntassets.com','5.imimg.com','rukminim1.flixcart.com','assets.ajio.com'],
+  domains: ['m.media-amazon.com','assets.myntassets.com','5.imimg.com','rukminim1.flixcart.com','assets.ajio.com'],i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  
 },} 
