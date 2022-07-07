7f2c3f1cefbd3274b49a57f61ac557e28f38d95a
Sachin <naiksci90@gmail.com>
2022-07-07 14:05:51 +0530

    Name fix commit 2

--- __tests__/Header.test.js
+++ __tests__/Header.test.js
@@ -1,55 +1,51 @@
-import { render, screen ,fireEvent} from '@testing-library/react'
-import { initialState } from '../context/reducer'
-import {AppWrapper} from "../context/StateProvider"
-import '@testing-library/jest-dom'
-import Header from "../components/Header"
-import Router from 'next/router'
+import { render, screen, fireEvent } from "@testing-library/react";
+import { initialState } from "../context/reducer";
+import { AppWrapper } from "../context/StateProvider";
+import "@testing-library/jest-dom";
+import Header from "../components/Header";
+import Router from "next/router";
 
+jest.mock("next/router", () => ({
+  push: jest.fn(),
+}));
 
-jest.mock('next/router',() =>({
-    push:jest.fn()
-}))
+initialState.basket = [
+  { id: 1, title: "Product Name", price: 500, quantity: 7 },
+];
 
-initialState.basket=[
-    {id:1 , title:"Product Name" , price:500 ,quantity:7}
-]
+describe("Checking Header Component", () => {
+  function renderHeader() {
+    return render(
+      <AppWrapper>
+        <Header />
+      </AppWrapper>
+    );
+  }
 
-describe('Checking Header Component', () => {
+  test("Should render Header  Elements ", () => {
+    renderHeader();
+    const Headerlement = screen.getByTestId("Header");
+    expect(Headerlement).toHaveTextContent("Ecom");
+    const InputBox = screen.getByLabelText("Search Box");
+    expect(InputBox).toHaveTextContent("");
+    const Cart = screen.getByLabelText("Cart Count");
+    expect(Cart).toHaveTextContent(7);
+  });
 
-    function renderHeader(){
-        return render(
-            <AppWrapper>
-             <Header/>
-             </AppWrapper>
-        )
-    }
+  test("Should render Correct Basket Quantity", () => {
+    renderHeader();
+    const Cart = screen.getByLabelText("Cart Count");
+    expect(Cart).toHaveTextContent(7);
+  });
 
-
-    test('Should render Header  Elements ' , () =>{
-        renderHeader();
-        const Headerlement = screen.getByTestId('Header');
-        expect(Headerlement).toHaveTextContent('Ecomm')
-        const InputBox = screen.getByLabelText('Search Box');
-        expect(InputBox).toHaveTextContent("");
-        const Cart = screen.getByLabelText('Cart Count');
-        expect(Cart).toHaveTextContent(7);
-    })
-
-    test('Should render Correct Basket Quantity',() =>{
-        renderHeader();
-        const Cart = screen.getByLabelText('Cart Count');
-        expect(Cart).toHaveTextContent(7);
-    })
-
-    test('Should search ',() =>{
-        renderHeader();
-        const InputBox = screen.getByPlaceholderText('Search for products,brands and more.');
-        fireEvent.change(InputBox,{target:{value:'laptop'}})
-        const SearchButton = screen.getByLabelText('Search Button');
-        fireEvent.click(SearchButton);
-        expect(Router.push).toHaveBeenCalledWith('/search/laptop')
-       
-    })
-
-
+  test("Should search ", () => {
+    renderHeader();
+    const InputBox = screen.getByPlaceholderText(
+      "Search for products,brands and more."
+    );
+    fireEvent.change(InputBox, { target: { value: "laptop" } });
+    const SearchButton = screen.getByLabelText("Search Button");
+    fireEvent.click(SearchButton);
+    expect(Router.push).toHaveBeenCalledWith("/search/laptop");
+  });
 });
