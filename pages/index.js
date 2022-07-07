2860dcc276c38404ce3f35a69fee9ff2c5191542
Sachin <naiksci90@gmail.com>
2022-07-07 13:41:32 +0530

    Name Check

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,31 @@
-import Head from "next/head"
-import ShowProducts from '../components/ShowProducts'
-import Slider from '../components/Slider'
+import Head from "next/head";
+import ShowProducts from "../components/ShowProducts";
+import Slider from "../components/Slider";
 
-export default function Home({data}) {
-  
+export default function Home({ data }) {
   return (
     <>
-    <Head>
-        <title>Ecomm</title>
+      <Head>
+        <title>Ecom</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-    <Slider/>
-    <div className='Home'>
-      <ShowProducts data={data} />
-    </div>
+      <Slider />
+      <div className="Home">
+        <ShowProducts data={data} />
+      </div>
     </>
-  )
+  );
 }
 
-export async function getServerSideProps(context)
-{ 
-    const url = 'http://'+context.req.headers.host + '/api/products';
-    const response = await fetch(url);
-    const data = await response.json()
+export async function getServerSideProps(context) {
+  const url = "http://" + context.req.headers.host + "/api/products";
+  const response = await fetch(url);
+  const data = await response.json();
 
-    return {
-      props:{
-          data:data
-      }
-    }
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
