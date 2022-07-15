f85fa407bb4a11cb4c983d38175cfc550ebe213a
Sachin Naik <sacnaik1@publicisgroupe.net>
2022-07-15 19:41:47 +0530

    Update link tag

--- pages/_document.js
+++ pages/_document.js
@@ -7,12 +7,6 @@ export default function Document() {
         <meta name="description" content="Ecomm wesbite made using NEXTJS" />
         <link
           rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
           href="https://use.fontawesome.com/releases/v6.0.0/css/all.css"
           integrity="sha384-3B6NwesSXE7YJlcLI9RpRqGf2p/EgVH8BgoKTaUrmKNDkHPStTQ3EyoYjCGXaOTS"
           crossOrigin="anonymous"
